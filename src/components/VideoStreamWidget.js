149a20a09ee3a0eb59fb93dba817227e34aa5ad0
Arshad Husain <arshad.husain1@gmail.com>
2022-03-08 02:05:48 -0700

    Make camera view fit widget container
    Allows for a larger pictures and less wasted space

--- src/components/VideoStreamWidget.js
+++ src/components/VideoStreamWidget.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react';
-
+import '../styles/VideoStreamWidget.css';
 import Grid from '@material-ui/core/Grid';
 
 import Widget from './Widget';
 
 
 export default class VideoStreamWidget extends Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
     return (
-      <Widget heading="Live Stream">
-        <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justify="center"
-        >
-          <img src="http://192.168.1.137:8081/2/stream" width="256" height="144" />
-        </Grid>
-      </Widget>
+      <div style={this.spanStyles} className="Widget1">
+        <img src="http://192.168.1.137:8081/4/stream" width="100%" height="auto" object-fit="contain" alt="description of image" />
+      </div>
     );
   }
 }
